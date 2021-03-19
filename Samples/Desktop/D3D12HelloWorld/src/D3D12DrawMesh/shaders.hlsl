ab744d614ea95b73f9b031f817e4219e5f776a4c
mag357num <45219797+Mag357num@users.noreply.github.com>
2021-03-19 10:49:52 +0800

    改shader错误

--- Samples/Desktop/D3D12HelloWorld/src/D3D12DrawMesh/shaders.hlsl
+++ Samples/Desktop/D3D12HelloWorld/src/D3D12DrawMesh/shaders.hlsl
@@ -21,7 +21,7 @@ struct PSInput
     float4 color : COLOR;
 };
 
-PSInput VSMain(float4 position : POSITION, float4 color : COLOR)
+PSInput VSMain(float3 position : POSITION, float4 color : COLOR)
 {
     PSInput result;
 
